2fa92c697e7b65b2414046cf986b4bd03863d82e
Jindrich Samec <jindrich.samec@xjs.cz>
2019-03-16 15:30:43 +0100

    qr code generator

--- src/middleware.ts
+++ src/middleware.ts
@@ -6,12 +6,13 @@ import * as limoList from './data/limo.json'
 const OAUTH_CLIENT_ID = getEnvVariable('OAUTH_CLIENT_ID')
 const ACCESS_TOKEN_COOKIE_NAME = getEnvVariable('ACCESS_TOKEN_COOKIE_NAME')
 const AWS_API_GATEWAY_STAGE = getEnvVariable('AWS_API_GATEWAY_STAGE', '')
+const LIMOALERT_SERVICE_BASE_URL = getEnvVariable('LIMOALERT_SERVICE_BASE_URL')
 
 export async function checkUserToken(ctx: Koa.Context, next: Function) {
   ctx.accessToken = ctx.cookies.get(ACCESS_TOKEN_COOKIE_NAME)
   let userCode = ctx.request.query.code
   console.log('Checking user token and code', ctx.accessToken, userCode)
-  const redirectUri = `${ctx.origin}${AWS_API_GATEWAY_STAGE}${ctx.path}`
+  const redirectUri = `${LIMOALERT_SERVICE_BASE_URL}${ctx.path}`
   if (userCode) {
     console.log('Request access in slack with code', userCode)
     ctx.accessToken = await fetchAccessToken(userCode, redirectUri)
@@ -35,7 +36,7 @@ export async function sendMessageToSlack(ctx: Koa.Context, next: Function) {
   }
   console.log('Sending message to slack with token', ctx.accessToken)
   await sendMessage(limo, ctx.accessToken)
-  ctx.redirect(`${AWS_API_GATEWAY_STAGE}/success/${limo}`)
+  ctx.redirect(`${LIMOALERT_SERVICE_BASE_URL}/success/${limo}`)
 
   await next()
 }
@@ -50,7 +51,8 @@ export async function renderSuccess(ctx: Koa.Context, next: Function) {
 
 export async function renderLimoList(ctx: Koa.Context, next: Function) {
   await ctx.render('list', {
-    limoList
+    limoList,
+    LIMOALERT_SERVICE_BASE_URL
   })
   ctx.response.status = 200
 }
